0000972ec8f1fb0d015daa24160e85868c8f4951
chadley antonels <chadleycantonels@gmail.com>
2024-08-05 21:49:10 +0200

    COMMIT

--- react-native ai-generated video app/app/index.jsx
+++ react-native ai-generated video app/app/index.jsx
@@ -7,11 +7,31 @@ export default function App () {
   return (
     <SafeAreaView className=" bg-primary h-full">
         <ScrollView contentContainerStyle={{ height:'100%' }}>
-          <View className=" w-full justify-center items-center
-           h-full px-4">
+          <View className=" w-full justify-start items-center
+           h-full px-4 mt-16">
             <Image source={images.logo}
                    className="w-[130px] h-[84px]"
                    resizeMode='contain'/>
+
+            <Image source={images.cards}
+                   className="max-w-[380px] h-[300px] w-full"
+                   resizeMode='contain'/> 
+
+            <View>
+                <Text className="text-3xl text-white font-bold text-center">
+                  Discover Endless Possibilities with {''}
+                  <Text className='text-secondary-200'> 
+                    Aora
+                  </Text>
+                </Text> 
+                <Image source={images.path}
+                       className='w-[136px] h-[15px] absolute -bottom-2 -top-8'
+                       resizeMode='contain'/>
+            </View>
+
+            <Text className='text-sm font-pregular text-gray-100 mt-7 text-center'>
+                Where creativity meets innovation: embark on a journey of limitless exploration with aora
+            </Text>       
            </View>
 
         </ScrollView>
---
+++ react-native ai-generated video app/components/CustomeButton.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CustomeButton = () => {
+  return (
+    <View>
+      <Text>CustomeButton</Text>
+    </View>
+  )
+}
+
+export default CustomeButton
